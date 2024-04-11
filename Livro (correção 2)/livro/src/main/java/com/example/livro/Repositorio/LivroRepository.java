a6b16a21f82f4b25ea84f2ff6ea6eadcdd551e1a
Josecarlos9021 <142549818+Josecarlos9021@users.noreply.github.com>
2024-04-11 12:09:15 -0300

    Add files via upload

---
+++ Livro (correção 2)/livro/src/main/java/com/example/livro/Repositorio/LivroRepository.java
@@ -0,0 +1,14 @@
+package com.example.livro.Repositorio;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.livro.Entidade.Livro;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    List<Livro> findByTitulo(String titulo);
+
+    boolean existsById(Long id);
+}
